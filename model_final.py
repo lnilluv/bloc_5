9ef8b9b98bb320c237c4c5af03b75ac83c2da4e7
lnilluv <laurent.vullin@gmail.com>
2023-04-10 13:38:08 +0200

    upd

--- model_final.py
+++ model_final.py
@@ -1,3 +1,100 @@
+# import os
+
+# import boto3
+# import mlflow
+# import numpy as np
+# import pandas as pd
+# from botocore.client import Config
+# from mlflow.models.signature import infer_signature
+# from sklearn.compose import ColumnTransformer
+# from sklearn.exceptions import ConvergenceWarning
+# from sklearn.impute import SimpleImputer
+# from sklearn.metrics import classification_report, mean_squared_error, r2_score
+# from sklearn.model_selection import GridSearchCV, train_test_split
+# from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import OneHotEncoder, StandardScaler
+# from xgboost import XGBRegressor
+
+# # Create a connection to S3 using the Boto3 library
+# s3 = boto3.resource('s3',
+#                     endpoint_url=os.getenv('MLFLOW_S3_ENDPOINT_URL'),
+#                     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+#                     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+#                     config=Config(signature_version='s3v4'),
+#                     region_name='us-east-1')
+
+# # Set your variables for your environment
+# EXPERIMENT_NAME = "getaround-bloc5"
+
+# # Set tracking URI to your Heroku application
+# mlflow.set_tracking_uri("https://mlflow.pryda.dev")
+
+# # Set experiment's info 
+# mlflow.set_experiment(EXPERIMENT_NAME)
+
+# # Get our experiment info
+# experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)
+
+# # Call mlflow autolog
+# mlflow.sklearn.autolog(log_models=False)
+
+# # Load the data
+# df = pd.read_csv('data/get_around_pricing_project.csv', index_col=0)
+
+# # Extract the features
+# X = df.drop('rental_price_per_day', axis=1)
+
+# # Extract the target column
+# y = df.loc[:, 'rental_price_per_day']
+
+# # Train / test split 
+# X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
+
+# # determine categorical and numerical features
+# numerical_features = X.select_dtypes(include=['int64', 'float64']).columns
+# categorical_features = X.select_dtypes(include=['object', 'bool']).columns
+
+# # Numerical Transformer
+# numerical_transformer = Pipeline([
+#     ('imputer', SimpleImputer(strategy='mean')),
+#     ('scaler', StandardScaler())
+# ])
+
+# # Categorical Transformer
+# categorical_transformer = Pipeline([
+#     ('imputer', SimpleImputer(strategy='most_frequent')),
+#     ('encoder', OneHotEncoder(drop='first', handle_unknown='ignore'))
+# ])
+
+# preprocessor = ColumnTransformer(
+#     transformers=[
+#         ("numerical_transformer", numerical_transformer, numerical_features),
+#         ("categorical_transformer", categorical_transformer, categorical_features)
+#     ]
+# )
+
+# # List of models
+# model = Pipeline([
+#         ('preprocessor', preprocessor),
+#         ('model', XGBRegressor(gamma=0, learning_rate=0.1, max_depth=10, min_child_weight=5, n_estimators=100))
+#     ])
+
+# # Log experiment to MLFlow
+# with mlflow.start_run(experiment_id=experiment.experiment_id):
+#     model.fit(X_train, y_train)
+#     predictions = model.predict(X_train)
+
+#     mlflow.log_metric("Train Score", model.score(X_train, y_train))
+#     mlflow.log_metric("Test Score", model.score(X_test, y_test))
+
+#     # Log model seperately to have more flexibility on setup
+#     mlflow.xgboost.log_model(
+#         xgb_model=model,
+#         artifact_path="getaround",
+#         registered_model_name="xgboost",
+#         signature=infer_signature(X_train, predictions)
+#     )
+
 import os
 
 import boto3
@@ -5,7 +102,9 @@ import mlflow
 import numpy as np
 import pandas as pd
 from botocore.client import Config
+from mlflow.models.signature import infer_signature
 from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import classification_report, mean_squared_error, r2_score
@@ -35,50 +134,64 @@ mlflow.set_experiment(EXPERIMENT_NAME)
 experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)
 
 # Call mlflow autolog
-mlflow.xgboost.autolog()
+mlflow.sklearn.autolog()
 
-# Start the experiment run
-with mlflow.start_run(experiment_id=experiment.experiment_id):
+# Load the data
+df = pd.read_csv('data/get_around_pricing_project.csv', index_col=0)
 
-    # Load the data
-    df = pd.read_csv('data/get_around_pricing_project.csv', index_col=0)
+# Extract the features
+X = df.drop('rental_price_per_day', axis=1)
 
-    # Extract the features
-    X = df.drop('rental_price_per_day', axis=1)
+# Extract the target column
+y = df.loc[:, 'rental_price_per_day']
 
-    # Extract the target column
-    y = df.loc[:, 'rental_price_per_day']
+# Train / test split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
 
-    # Train / test split 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
+# determine categorical and numerical features
+numerical_features = X.select_dtypes(include=['int64', 'float64']).columns
+categorical_features = X.select_dtypes(include=['object', 'bool']).columns
 
-    # determine categorical and numerical features
-    numerical_features = X.select_dtypes(include=['int64', 'float64']).columns
-    categorical_features = X.select_dtypes(include=['object', 'bool']).columns
+# Numerical Transformer
+numerical_transformer = Pipeline([
+    ('imputer', SimpleImputer(strategy='mean')),
+    ('scaler', StandardScaler())
+])
 
-    # Numerical Transformer
-    numerical_transformer = Pipeline([
-        ('imputer', SimpleImputer(strategy='mean')),
-        ('scaler', StandardScaler())
-    ])
+# Categorical Transformer
+categorical_transformer = Pipeline([
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('encoder', OneHotEncoder(drop='first', handle_unknown='ignore'))
+])
 
-    # Categorical Transformer
-    categorical_transformer = Pipeline([
-        ('imputer', SimpleImputer(strategy='most_frequent')),
-        ('encoder', OneHotEncoder(drop='first', handle_unknown='ignore'))
-    ])
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("numerical_transformer", numerical_transformer, numerical_features),
+        ("categorical_transformer", categorical_transformer, categorical_features)
+    ]
+)
 
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ("numerical_transformer", numerical_transformer, numerical_features),
-            ("categorical_transformer", categorical_transformer, categorical_features)
-        ]
+# List of models
+model = Pipeline(
+    steps=
+    [
+        ('preprocessor', preprocessor),
+        ('model', GradientBoostingRegressor(learning_rate=0.1, max_depth=5, min_samples_split=10, n_estimators=200))
+    ]
     )
 
-    # List of models
-    model = Pipeline([
-            ('preprocessor', preprocessor),
-            ('model', XGBRegressor(gamma=0, learning_rate=0.1, max_depth=10, min_child_weight=5, n_estimators=100))
-        ])
-
+# Log experiment to MLFlow
+with mlflow.start_run(experiment_id=experiment.experiment_id):
     model.fit(X_train, y_train)
+    predictions = model.predict(X_train)
+
+    mlflow.log_metric("Train Score", model.score(X_train, y_train))
+    mlflow.log_metric("Test Score", model.score(X_test, y_test))
+
+    # Log model
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="getaround",
+        registered_model_name="gradienboosting",
+        signature=infer_signature(X_train, predictions)
+    )
